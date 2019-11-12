8b0aa2f451da6f7c858756059f5e64ecd7fdcbf4
Vladimir <UglyBob23@yandex.ru>
2019-11-12 19:07:06 +0500

    Initial Commit

---
+++ letsTryConstraintsAppTests/letsTryConstraintsAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  letsTryConstraintsAppTests.swift
+//  letsTryConstraintsAppTests
+//
+//  Created by Владимир Паутов on 12.11.2019.
+//  Copyright © 2019 Владимир Паутов. All rights reserved.
+//
+
+import XCTest
+@testable import letsTryConstraintsApp
+
+class letsTryConstraintsAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
